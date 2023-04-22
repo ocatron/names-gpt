35e103c8f7cfc90333591f84140e79f742fc33b7
Thamara Liyanage <thamara.prabuddha@gmail.com>
2023-04-22 21:05:48 +0530

    Configure tailwind to include alt-lg shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { sans: ['var(--font-poppins)', ...fontFamily.sans] },
+      boxShadow: {
+        'alt-lg': 'rgba(0, 0, 0, 0.15) 0px 5px 15px 0px',
+      },
     },
   },
   plugins: [],
